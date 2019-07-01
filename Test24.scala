170ca78a0486467f6d20d9205f2d5de5b55155f8
RajithaGayashan <rajithagayashan95@gmail.com>
2019-07-01 12:30:25 +0530

    Array(1)

---
+++ Test24.scala
@@ -0,0 +1,11 @@
+object Test24{
+    def main(arrgs:Array[String]){
+        var myarray:Array[String]=new Array[String](3);
+        myarray(0)="Rajitha";
+        myarray(1)="Gayashan";
+        myarray(2)="rajithagayashan95@gmail.com";
+        for(x<-myarray){
+            println(x);
+        }
+    }
+}
