11b6b928361592639c3a8d31d333811135f6ce9f
RajithaGayashan <rajithagayashan95@gmail.com>
2019-07-08 23:58:33 +0530

    String interpolation(s,f,raw)

---
+++ Test29.scala
@@ -0,0 +1,12 @@
+object Test29{
+    def main(args:Array[String]){
+        val name="Rajitha";
+        val age=23;
+        val salary=34556.6875;
+        println(s"Name=$name  Age=$age");
+        println(f"Name=$name Age=$age");
+        println("e\nf   g\th");
+        println();
+        println(raw"a\nb   c\td");
+        }
+} 
